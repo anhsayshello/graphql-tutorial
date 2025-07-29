a5a517034e7c28166af91e340f9245bc488f386f
anhsayshello <anhle0347@gmail.com>
2025-07-30 00:01:00 +0700

    feat: add user schema

---
+++ server/graphql/schema/user.schema.js
@@ -0,0 +1,26 @@
+const userSchema = `
+    type User {
+  username: String!
+  friends: [Person!]!
+  id: ID!
+}
+
+type Token {
+  value: String!
+}
+
+type Query {
+  me: User
+}
+
+type Mutation {
+  createUser(
+    username: String!
+  ): User
+  login(
+    username: String!
+    password: String!
+  ): Token
+}
+`;
+export default userSchema;
