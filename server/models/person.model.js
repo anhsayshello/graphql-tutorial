e7661f02bbe22cde95c8bf72e27f5db98d4d1d7c
anhsayshello <anhle0347@gmail.com>
2025-07-30 00:00:44 +0700

    feat: add person model

---
+++ server/models/person.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  phone: {
+    type: String,
+    minlength: 5,
+  },
+  street: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  city: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+});
+
+export default mongoose.model("Person", personSchema);
