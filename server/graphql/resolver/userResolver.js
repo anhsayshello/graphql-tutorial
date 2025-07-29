1cac2fb9e7651a109ad673dd755cbc5e3b44acbb
anhsayshello <anhle0347@gmail.com>
2025-07-30 00:02:03 +0700

    feat: add resolver: me, createUser, login

---
+++ server/graphql/resolver/userResolver.js
@@ -0,0 +1,49 @@
+import { GraphQLError } from "graphql";
+import User from "../../models/user.model.js";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../../config/config.js";
+
+const userResolver = {
+  Query: {
+    me: (root, args, { currentUser }) => {
+      return currentUser;
+    },
+  },
+
+  Mutation: {
+    createUser: async (root, args) => {
+      const user = new User({ username: args.username });
+
+      return user.save().catch((error) => {
+        throw new GraphQLError("Creating the user failed", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            invalidArgs: args.username,
+            error,
+          },
+        });
+      });
+    },
+
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username });
+
+      if (!user || args.password !== "secret") {
+        throw new GraphQLError("wrong credentials", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+          },
+        });
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      };
+
+      return { value: jwt.sign(userForToken, JWT_SECRET) };
+    },
+  },
+};
+
+export default userResolver;
