cf88fc80accc727cb7a2be5fa4ec294576641bca
anhsayshello <anhle0347@gmail.com>
2025-07-30 00:15:17 +0700

    refactor: add token to a header

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,37 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import "@/index.css";
 import App from "@/App.tsx";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { getTokenFromLS } from "./utils/utils";
+
+const authLink = setContext((_, { headers }) => {
+  const token = getTokenFromLS();
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null,
+    },
+  };
+});
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000",
+});
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
   cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
 });
 
 createRoot(document.getElementById("root")!).render(
