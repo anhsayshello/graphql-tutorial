d27dbf4731960fe4965f31d5e2e30d808c0d7963
anhsayshello <anhle0347@gmail.com>
2025-07-30 00:07:12 +0700

    refactor: to separate file

--- server/index.js
+++ server/index.js
@@ -1,262 +1,13 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { GraphQLError } from "graphql";
-import { v1 as uuid } from "uuid";
+import typeDefs from "./graphql/schema/index.js";
+import resolvers from "./graphql/resolver/index.js";
+import connectDB from "./config/db.js";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config/config.js";
+import User from "./models/user.model.js";
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    phone: "040-123543",
-    street: "Tapiolankatu 5 A",
-    city: "Espoo",
-    id: "3d594650-3436-11e9-bc57-8b80ba54c431",
-  },
-  {
-    name: "Matti Luukkainen",
-    phone: "040-432342",
-    street: "Malminkaari 10 A",
-    city: "Helsinki",
-    id: "3d599470-3436-11e9-bc57-8b80ba54c431",
-  },
-  {
-    name: "Venla Ruuska",
-    street: "Nallemäentie 22 C",
-    city: "Helsinki",
-    id: "3d599471-3436-11e9-bc57-8b80ba54c431",
-  },
-];
-
-const typeDefs = `
-  type Address {
-    street: String!
-    city: String!
-  }
-
-  enum YesNo{
-  YES
-  NO
-  }
-
-  type Person {
-  name: String!
-  phone:String
-  address: Address!
-  id: ID!
-  }
-
-  type Query{
-  personCount: Int!
-  allPersons(phone: YesNo): [Person!]!
-  findPerson(name: String!): Person
-  }
-
-  type Mutation{
-    addPerson(
-    name: String!
-    phone: String
-    street: String!
-    city: String
-    ):Person
-
-    editNumber(
-    name:String!
-    phone:String!):Person
-  }
-`;
-const resolvers = {
-  Query: {
-    personCount: () => persons.length,
-    allPersons: (root, args) => {
-      if (!args.phone) {
-        return persons;
-      }
-      const byPhone = (person) =>
-        args.phone === "YES" ? person.phone : !person.phone;
-      return persons.filter(byPhone);
-    },
-    findPerson: (root, args) => persons.find((p) => p.name === args.name),
-  },
-  Mutation: {
-    addPerson: (root, args) => {
-      if (persons.find((p) => p.name === args.name)) {
-        throw new GraphQLError("Name must be unique", {
-          extensions: {
-            code: "BAD_USER_INPUT",
-            invalidArgs: args.name,
-          },
-        });
-      }
-      const person = { ...args, id: uuid() };
-      persons = persons.concat(person);
-      return person;
-    },
-    editNumber: (root, args) => {
-      const person = persons.find((p) => p.name === args.name);
-      const updatedPerson = { ...person, phone: args.phone };
-      persons = persons.map((p) => (p.name === args.name ? updatedPerson : p));
-      return updatedPerson;
-    },
-  },
-  Person: {
-    address: (root) => ({
-      street: root.street,
-      city: root.city,
-    }),
-  },
-};
-
-// let authors = [
-//   {
-//     name: "Robert Martin",
-//     id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-//     born: 1952,
-//   },
-//   {
-//     name: "Martin Fowler",
-//     id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-//     born: 1963,
-//   },
-//   {
-//     name: "Fyodor Dostoevsky",
-//     id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-//     born: 1821,
-//   },
-//   {
-//     name: "Joshua Kerievsky", // birthyear not known
-//     id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-//   },
-//   {
-//     name: "Sandi Metz", // birthyear not known
-//     id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-//   },
-// ];
-
-// let books = [
-//   {
-//     title: "Clean Code",
-//     published: 2008,
-//     author: "Robert Martin",
-//     id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-//     genres: ["refactoring"],
-//   },
-//   {
-//     title: "Agile software development",
-//     published: 2002,
-//     author: "Robert Martin",
-//     id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-//     genres: ["agile", "patterns", "design"],
-//   },
-//   {
-//     title: "Refactoring, edition 2",
-//     published: 2018,
-//     author: "Martin Fowler",
-//     id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-//     genres: ["refactoring"],
-//   },
-//   {
-//     title: "Refactoring to patterns",
-//     published: 2008,
-//     author: "Joshua Kerievsky",
-//     id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-//     genres: ["refactoring", "patterns"],
-//   },
-//   {
-//     title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
-//     published: 2012,
-//     author: "Sandi Metz",
-//     id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-//     genres: ["refactoring", "design"],
-//   },
-//   {
-//     title: "Crime and punishment",
-//     published: 1866,
-//     author: "Fyodor Dostoevsky",
-//     id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-//     genres: ["classic", "crime"],
-//   },
-//   {
-//     title: "Demons",
-//     published: 1872,
-//     author: "Fyodor Dostoevsky",
-//     id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-//     genres: ["classic", "revolution"],
-//   },
-// ];
-
-// const typeDefs = `
-//   type Author{
-//     name: String!
-//     id: ID!
-//     born: Int
-//     bookCount: Int
-//   }
-
-//   type Book{
-//     title:String!
-//     author: String!
-//     published: Int!
-//     id: ID!
-//     genres: [String!]!
-//   }
-
-//   type Query {
-//     bookCount: Int!
-//     authorCount: Int!
-//     allBooks(author: String, genre: String): [Book!]!
-//     allAuthors: [Author!]!
-//   }
-
-//   type Mutation{
-//     addBook(title:String!, author: String!, published: Int!, genres: [String!]!): Book!
-//     editAuthor(name: String!, setBornTo: Int!): Author
-//   }
-// `;
-
-// const resolvers = {
-//   Query: {
-//     bookCount: () => books.length,
-//     authorCount: () => authors.length,
-//     allBooks: (root, args) => {
-//       return books.filter((book) => {
-//         const matchesAuthor = args.author ? book.author === args.author : true;
-//         const matchesGenre = args.genre
-//           ? book.genres.includes(args.genre)
-//           : true;
-//         return matchesAuthor && matchesGenre;
-//       });
-//     },
-//     allAuthors: () => {
-//       for (let i = 0; i < authors.length; i++) {
-//         authors[i].bookCount = 0;
-//         books.forEach((b) => {
-//           if (b.author === authors[i].name) {
-//             authors[i].bookCount += 1;
-//           }
-//         });
-//       }
-//       return authors;
-//     },
-//   },
-
-//   Mutation: {
-//     addBook: (root, args) => {
-//       const book = { ...args, id: uuid() };
-//       books = books.concat(book);
-//       return book;
-//     },
-//     editAuthor: (root, args) => {
-//       const author = authors.find((a) => a.name === args.name);
-//       if (!author) {
-//         return null;
-//       }
-//       const updatedAuthor = { ...author, born: args.setBornTo };
-//       authors = authors.map((author) =>
-//         author.name === args.name ? updatedAuthor : author
-//       );
-//       return updatedAuthor;
-//     },
-//   },
-// };
+connectDB();
 
 const server = new ApolloServer({
   typeDefs,
@@ -265,6 +16,16 @@ const server = new ApolloServer({
 
 startStandaloneServer(server, {
   listen: { port: 4000 },
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null;
+    if (auth && auth.startsWith("Bearer ")) {
+      const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET);
+      const currentUser = await User.findById(decodedToken.id).populate(
+        "friends"
+      );
+      return { currentUser };
+    }
+  },
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
