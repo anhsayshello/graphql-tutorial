8c5344e42a65f3a3a0e1f61a4a2b3c3f1b2643d6
anhsayshello <anhle0347@gmail.com>
2025-07-31 00:02:14 +0700

    feat: add updateCache logic function

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -1,3 +1,21 @@
 export const setTokenToLS = (token: string) =>
   localStorage.setItem("token", token);
 export const getTokenFromLS = () => localStorage.getItem("token");
+
+// function that takes care of manipulating cache
+export const updateCache = (cache, query, addedPerson) => {
+  // helper that is used to eliminate saving same person twice
+  const uniqByName = (a) => {
+    let seen = new Set();
+    return a.filter((item) => {
+      let k = item.name;
+      return seen.has(k) ? false : seen.add(k);
+    });
+  };
+
+  cache.updateQuery(query, ({ allPersons }) => {
+    return {
+      allPersons: uniqByName(allPersons.concat(addedPerson)),
+    };
+  });
+};
